08ccffe6c64e491381225fe7fd6c60105eb0626a
Juan Göttling <juangottling@gmail.com>
2020-07-27 17:55:57 -0300

    created class Product with attributes and setters/getters

---
+++ e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/model/Product.java
@@ -0,0 +1,47 @@
+package ar.com.gl.shop.product.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Stock stock;
+//    private Category category;
+	
+    
+    //constructors
+    
+    //setters and getters
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Stock getStock() {
+		return stock;
+	}
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+  
+    
+}
