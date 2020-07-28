230ca5c20c28037d5bbce4aef83baeda5197c944
Juan Göttling <juangottling@gmail.com>
2020-07-28 15:19:07 -0300

    update stock crud methods

--- e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/services/impl/StockServiceImpl.java
+++ e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/services/impl/StockServiceImpl.java
@@ -6,19 +6,25 @@ public class StockServiceImpl implements StockService{
 
 	//CRUD methods
 	public void createStock(Long id, Integer quantity, String locationCode) {
-		
+		//Repository.getInstance().createStock(Long id, Integer quantity, String locationCode)
 	}
 	
 	public void getStock(Long id) {
-		
+		//Repository.getInstance().getStock(Long id)
+	}
+	
+	public void getAllStocks() {
+		//List<Stock> allStocks = Repository.getInstance().getAllStocks()
+		//TODO print all stocks
 	}
 	
 	public void updateStock(Long id, Integer quantity, String locationCode) {
-		
+		//Repository.getInstance().updateStock(Long id, Integer quantity, String locationCode)
 	}
 	
 	public void deleteStock(Long id) {
 		
 		//TODO
+		//Repository...
 	}
 }
