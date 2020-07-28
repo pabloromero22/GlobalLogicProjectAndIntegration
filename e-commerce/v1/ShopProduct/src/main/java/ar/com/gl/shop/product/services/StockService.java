1c3b03b1c5be1be0e715a5e6d4591090f1c9e843
Juan Göttling <juangottling@gmail.com>
2020-07-28 16:22:34 -0300

    update stock service

--- e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/services/StockService.java
+++ e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/services/StockService.java
@@ -1,5 +1,16 @@
 package ar.com.gl.shop.product.services;
 
+import java.util.List;
+
+import ar.com.gl.shop.product.model.Stock;
+
 public interface StockService {
 
+	//CRUD methods
+	public void createStock(Stock stock);
+	public Stock getStock(Long id);
+	public List<Stock> getAllStocks();
+	public void updateStock(Stock updatedStock);
+	public void deleteStock(Long id);
+	
 }
--- e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/services/impl/StockServiceImpl.java
+++ e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/services/impl/StockServiceImpl.java
@@ -1,30 +1,48 @@
 package ar.com.gl.shop.product.services.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.com.gl.shop.product.model.Stock;
 import ar.com.gl.shop.product.services.StockService;
 
 public class StockServiceImpl implements StockService{
-
+//	private StockRepository repository;
+	
+//	public StockServiceImpl(StockRepository repository) {
+//		this.repository = repository;
+//	}
+	
 	//CRUD methods
-	public void createStock(Long id, Integer quantity, String locationCode) {
+	@Override
+	public void createStock(Stock stock) {
 		//Repository.getInstance().createStock(Long id, Integer quantity, String locationCode)
 	}
 	
-	public void getStock(Long id) {
+	@Override
+	public Stock getStock(Long id) {
+		return new Stock();
 		//Repository.getInstance().getStock(Long id)
 	}
 	
-	public void getAllStocks() {
+	@Override
+	public List<Stock> getAllStocks() {
+		
+		return new ArrayList<Stock>();
 		//List<Stock> allStocks = Repository.getInstance().getAllStocks()
 		//TODO print all stocks
 	}
 	
-	public void updateStock(Long id, Integer quantity, String locationCode) {
+	@Override
+	public void updateStock(Stock updatedStock) {
 		//Repository.getInstance().updateStock(Long id, Integer quantity, String locationCode)
 	}
 	
+	@Override
 	public void deleteStock(Long id) {
 		
 		//TODO
 		//Repository...
 	}
+
 }
