dbb823af25dcfeef46809310e0e5788d93111bee
Juan Göttling <juangottling@gmail.com>
2020-07-28 15:14:43 -0300

    stock crud methods

--- e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/services/impl/StockServiceImpl.java
+++ e-commerce/v1/ShopProduct/src/main/java/ar/com/gl/shop/product/services/impl/StockServiceImpl.java
@@ -4,4 +4,21 @@ import ar.com.gl.shop.product.services.StockService;
 
 public class StockServiceImpl implements StockService{
 
+	//CRUD methods
+	public void createStock(Long id, Integer quantity, String locationCode) {
+		
+	}
+	
+	public void getStock(Long id) {
+		
+	}
+	
+	public void updateStock(Long id, Integer quantity, String locationCode) {
+		
+	}
+	
+	public void deleteStock(Long id) {
+		
+		//TODO
+	}
 }
